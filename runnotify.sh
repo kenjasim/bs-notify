ad4bef3570f080448efc39ad810c6db8d476adc5
Kenan Jasim <kenan@Kenans-MacBook-Pro.local>
2021-08-19 13:33:42 +0100

    fixed comments

--- runnotify.sh
+++ runnotify.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
-####
+########################################################
 # Shell script to run a notification on apple computers
 # which remind you to test your blood sugar
-####
+########################################################
+
 
 # Function to create the blood glucose notification on mac
 function create_notification(){
     osascript -e 'display notification "Test your blood sugar" with title "Blood Sugar Alert"'
 }
 
+# Run the notification
 create_notification
